f82cbbe12a88172a9aa1308c6843a0932f2f7853
Jeneth Diesta <jen6one9@gmail.com>
2020-08-08 22:47:14 -0700

    updated the read me

--- README.md
+++ README.md
@@ -2,8 +2,7 @@
 This project seeks to deliver a simple weather dashboard that leverages the OpenWeather API to retrieve weather data for cities.
 When users search for a city, they are presented with a 5 day weather forecast and the UI displays pertinent weather information for that city, including a UV index with a color indicator informing the user whether conditions are favorable, moderate or severe. 
 
-The city is then added to the search history, appearing within the last searched city section on the UI. 
-
+The city is then added to the search history, allowing the user to revist past searches.
 
 ## User Story
 As a traveler, I want to see the weather outlook for multiple cities so that I can plan for a trip accordingly. 
--- script.js
+++ script.js
@@ -2,31 +2,31 @@
 $("#searchBtn").on("click", function () {
     var APIKey = "277c25e400c8e8f8bdc2d5b9d65d0ce7";
     var queryURL = "api.openweathermap.org/data/2.5/weather?q=London,uk&appid=" + APIKey;
-    
+
 
     $.ajax({
         url: queryURL,
-        method: "GET"
-
-    }).then(function (response) {
+        method: "GET",
         console.log(queryURL);
+    }).then(function (response) {
+
         console.log(response);
 
         const results = response.respone.docs;
         const weather = $('#results');
         const temp = (response.main.temp - 273.15) * 1.80 + 32
 
-
-        $(".city").html("<h1>" + response.name + "Weather Details</h1>");
+        $(".city").html("<h1>" + response.name + "Weather Details</h1>"); 
         $(".wind").text("Wind Speed: " + response.wind.speed);
+        console.log("Wind Speed: " + response.wind.speed);
         $(".humidity").text("Humidity: " + response.main.humidity);
+        console.log("Humidity: " + response.main.humidity);
 
         var tempF = (response.main.temp - 273.15) * 1.80 + 32
 
         $(".temp").text("Temperature (K) " + response.main.temp);
         $(".tempF").text("Temperature (F) " + tempF.toFixed(2));
-
-        console.log("Wind Speed: " + response.wind.speed);
-        console.log("Humidity: " + response.main.humidity);
         console.log("Temperature (F): " + tempF);
+
+
     })
