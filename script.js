66d193f08812ac0431afbf8a9fd122dc7de8715d
Jeneth Diesta <jen6one9@gmail.com>
2020-08-08 23:51:22 -0700

    added UL for search history

--- script.js
+++ script.js
@@ -4,31 +4,49 @@ function displayDate() {
     currentDateEl.text(currentDate)
     console.log("currentDate", currentDate)
 
-$("#searchBtn").on("click", function () {
-    var APIKey = "277c25e400c8e8f8bdc2d5b9d65d0ce7";
-    var queryURL = "api.openweathermap.org/data/2.5/forecast?id={city ID}&appid=277c25e400c8e8f8bdc2d5b9d65d0ce7"
+    function myFunction() {
+        var input, filter, ul, li, a, i, txtValue;
+        input = document.getElementById("userInput");
+        filter = input.value.toUpperCase();
+        ul = document.getElementById("searchUL");
+        li = ul.getElementsByTagName("li");
+        for (i = 0; i < li.length; i++) {
+            a = li[i].getElementsByTagName("a")[0];
+            txtValue = a.textContent || a.innerText;
+            if (txtValue.toUpperCase().indexOf(filter) > -1) {
+                li[i].style.display = "";
+            } else {
+                li[i].style.display = "none";
+            }
+        }
+    }
 
-    $.ajax({
-        url: queryURL,
-        method: "GET",
-         
-    }).then(function (response) {
 
-        console.log(response);
+    $("#searchBtn").on("click", function () {
+        var APIKey = "277c25e400c8e8f8bdc2d5b9d65d0ce7";
+        var queryURL = "api.openweathermap.org/data/2.5/forecast?id={city ID}&appid=277c25e400c8e8f8bdc2d5b9d65d0ce7"
 
-        const results = response.respone.docs;
-        const weather = $('#results');
-        const temp = (response.main.temp - 273.15) * 1.80 + 32
+        $.ajax({
+            url: queryURL,
+            method: "GET",
 
-        $(".city").html("<h1>" + response.name + "Weather Details</h1>"); 
-        $(".wind").text("Wind Speed: " + response.wind.speed);
-        console.log("Wind Speed: " + response.wind.speed);
-        $(".humidity").text("Humidity: " + response.main.humidity);
-        console.log("Humidity: " + response.main.humidity);
+        }).then(function (response) {
 
-        var tempF = (response.main.temp - 273.15) * 1.80 + 32
+            console.log(response);
 
-        $(".temp").text("Temperature (K) " + response.main.temp);
-        $(".tempF").text("Temperature (F) " + tempF.toFixed(2));
-        console.log("Temperature (F): " + tempF);
-    })
+            const results = response.respone.docs;
+            const weather = $('#results');
+            const temp = (response.main.temp - 273.15) * 1.80 + 32
+
+            $(".city").html("<h1>" + response.name + "Weather Details</h1>");
+            $(".wind").text("Wind Speed: " + response.wind.speed);
+            console.log("Wind Speed: " + response.wind.speed);
+            $(".humidity").text("Humidity: " + response.main.humidity);
+            console.log("Humidity: " + response.main.humidity);
+
+            var tempF = (response.main.temp - 273.15) * 1.80 + 32
+
+            $(".temp").text("Temperature (K) " + response.main.temp);
+            $(".tempF").text("Temperature (F) " + tempF.toFixed(2));
+            console.log("Temperature (F): " + tempF);
+        })
