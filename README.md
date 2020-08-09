13fdd9e39ac0a7599ee03124e29fb26e3419c8de
Jeneth Diesta <jen6one9@gmail.com>
2020-08-08 21:49:30 -0700

    update the readme

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 # that-weather-dashboard
-Simple weather dashboard that leverages the OpenWeather API to retrieve weather data for cities.
+This project seeks to deliver a simple weather dashboard that leverages the OpenWeather API to retrieve weather data for cities.
+When users search for a city, they are presented with a 5 day weather forecast and the UI displays pertinent weather information for that city, including a UV index with a color indicator informing the user whether conditions are favorable, moderate or severe. 
+
+The city is then added to the search history, appearing within the last searched city section on the UI. 
+
+
+## User Story
+As a traveler, I want to see the weather outlook for multiple cities so that I can plan for a trip accordingly. 
+
+
+
+## Preview
+![alt-text](readme.xxx)
+
+## Deployed Site Link
+Here's the link to my app:   
+
+## Acknowledgements
+This successful launch of this application was made possible through the support of my tutor and cohort peers. 
+
--- script.js
+++ script.js
@@ -1,28 +1,32 @@
+
+$("#searchBtn").on("click", function () {
     var APIKey = "277c25e400c8e8f8bdc2d5b9d65d0ce7";
-    var queryURL = "api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=277c25e400c8e8f8bdc2d5b9d65d0ce7"
+    var queryURL = "api.openweathermap.org/data/2.5/weather?q=London,uk&appid=" + APIKey;
+    
 
     $.ajax({
         url: queryURL,
         method: "GET"
 
-    }).then(function(response {
+    }).then(function (response) {
         console.log(queryURL);
-        console.log(resonse);
-        const temp = (response.main.temp-273.15)*1.80+32
-        console.logIte
+        console.log(response);
 
-        $(".city").html("<h1>" + resonse.name + "Weather Details</h1>");
-        $(".wind").text("Wind Speed: " + resonse.wind.speed);
+        const results = response.respone.docs;
+        const weather = $('#results');
+        const temp = (response.main.temp - 273.15) * 1.80 + 32
+
+
+        $(".city").html("<h1>" + response.name + "Weather Details</h1>");
+        $(".wind").text("Wind Speed: " + response.wind.speed);
         $(".humidity").text("Humidity: " + response.main.humidity);
 
-        var tempF = (resonse.main.temp-273.15)*1.80+32
+        var tempF = (response.main.temp - 273.15) * 1.80 + 32
 
         $(".temp").text("Temperature (K) " + response.main.temp);
         $(".tempF").text("Temperature (F) " + tempF.toFixed(2));
 
-        console.log("Wind Speed: "+ response.wind.speed);
+        console.log("Wind Speed: " + response.wind.speed);
         console.log("Humidity: " + response.main.humidity);
         console.log("Temperature (F): " + tempF);
-    }
-
-    
+    })
