03d6bd2107458d100d2976ff08476b3aa88f831d
Jeneth Diesta <jen6one9@gmail.com>
2020-08-08 23:34:09 -0700

    fixed the layout and added 5 place holders for the 5 day forecast

--- script.js
+++ script.js
@@ -1,13 +1,17 @@
+function displayDate() {
+    const currentDate = moment().format('dddd, MMMM DD, YYYY')
+    const currentDateEl = $("#currentDay");
+    currentDateEl.text(currentDate)
+    console.log("currentDate", currentDate)
 
 $("#searchBtn").on("click", function () {
     var APIKey = "277c25e400c8e8f8bdc2d5b9d65d0ce7";
-    var queryURL = "api.openweathermap.org/data/2.5/weather?q=London,uk&appid=" + APIKey;
-
+    var queryURL = "api.openweathermap.org/data/2.5/forecast?id={city ID}&appid=277c25e400c8e8f8bdc2d5b9d65d0ce7"
 
     $.ajax({
         url: queryURL,
         method: "GET",
-        console.log(queryURL);
+         
     }).then(function (response) {
 
         console.log(response);
@@ -27,6 +31,4 @@ $("#searchBtn").on("click", function () {
         $(".temp").text("Temperature (K) " + response.main.temp);
         $(".tempF").text("Temperature (F) " + tempF.toFixed(2));
         console.log("Temperature (F): " + tempF);
-
-
     })
